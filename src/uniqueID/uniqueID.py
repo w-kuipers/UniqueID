9cdf0a4e2da88f79e2ad60cfe7609c71286a4d03
Wibo Kuipers <w.kuipers@filmage.nl>
2022-01-08 12:00:38 +0100

    first commit

---
+++ src/uniqueID/uniqueID.py
@@ -0,0 +1,44 @@
+import string as _string
+import random
+
+#### 0123456789
+lettersAndDigits = _string.digits
+
+#### Return random integer value
+def integer(length=6, prefix=None):
+
+    #### Prefix should be of type int
+    if not isinstance(prefix, int):
+        raise Exception('Prefix should be of type int')
+    
+    randomInteger = int(''.join(random.choice(lettersAndDigits) for i in range(length)))
+
+    if not prefix == None:
+        randomInteger = prefix + randomInteger
+
+    return randomInteger
+
+#### Return random string value
+def string(length=6, prefix=None):
+    randomString = int(''.join(random.choice(lettersAndDigits) for i in range(length)))
+
+    if not prefix == None:
+        randomString = prefix + str(randomString)
+
+    return str(randomString)
+
+
+
+#### Function to create a unique ID for in the database....
+# def uniqueID(table, column, stringLength=6, prefix=None):
+#     idExists = True
+#     while idExists:
+#         randomID = self.randomStringDigits(stringLength, prefix=prefix)
+
+#         with connections['FMM'].cursor() as cursor:
+#             cursor.execute('SELECT * FROM {} WHERE {} = "{}"'.format(table, column, randomID))
+
+#             if not len(cursor.fetchall()):
+#                 idExists = False
+    
+#     return randomID
