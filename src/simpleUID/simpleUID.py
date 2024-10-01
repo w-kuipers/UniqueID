319bf83d732b6643ddb8b116ddcca6ced6ce0b35
Wibo Kuipers <wibo@dtchdigitals.com>
2024-10-01 17:23:10 +0200

    Remove useless comments

--- src/simpleUID/simpleUID.py
+++ src/simpleUID/simpleUID.py
@@ -1,22 +1,18 @@
 import random
 import secrets
-import string as _string  # # Function string is defined in code
+import string as _string
 from datetime import datetime
 from typing import Optional
 
 from .include import length_check
 
-#### 0123456789
-digits = _string.digits
+digits = _string.digits ## 0123456789
 
 
 #### Return random integer value
 def integer(length: int = 6, prefix: Optional[int] = None, ignore_max_length: bool = False):
+    length_check(length, ignore_max_length)
 
-    #### Check if specified length is allowed
-    length_check(length, ignore_max_length)  ## Will fail if returns True
-
-    #### Prefix should be of type int
     if not isinstance(prefix, int):
         if not prefix == None:
             raise TypeError("Prefix should be of type int")
@@ -53,27 +49,20 @@ def string(
     uppercase_only=False,
     lowercase_only=False,
 ):
+    length_check(length, ignore_max_length)
 
-    #### Check if specified length is allowed
-    length_check(length, ignore_max_length)  ## Will fail if returns True
-
-    #### Prefix should be of type str
     if not isinstance(prefix, str):
         if not prefix == None:
             raise TypeError("Prefix should be of type str")
 
-    #### String can't be both uppercase only and lowercase only
     if uppercase_only and lowercase_only:
         raise ValueError(
             "A string can not be uppercase only and lowercase only at the same time"
         )
 
-    #### Check if 'type' is 'integer'
     if type == "number":
-        generated = str(integer(length=length))  ## Why write duplicate code?
+        generated = str(integer(length=length))
     else:
-
-        #### Check for upperscase_only or lowercase_only
         alphabet = (
             _string.ascii_uppercase
             if uppercase_only
@@ -87,7 +76,6 @@ def string(
         )
         generated = "".join(secrets.choice(choices) for i in range(length))
 
-    #### Add prefix
     if not prefix == None:
         generated = prefix + generated
 
@@ -95,9 +83,7 @@ def string(
 
 
 def password(length: int = 10, ignore_max_length: bool = False, symbols=True):
-
-    #### Check if specified length is allowed
-    length_check(length, ignore_max_length)  ## Will fail if returns True
+    length_check(length, ignore_max_length)
 
     alphabet = _string.ascii_letters + digits
     if symbols:
@@ -117,35 +103,26 @@ def password(length: int = 10, ignore_max_length: bool = False, symbols=True):
 
 #### Generate random byte string (Basicly a wrapper for secrets.token_bytes)
 def bytes(length: int = 32, ignore_max_length: bool = False):
-
-    #### Check if specified length is allowed
-    length_check(length, ignore_max_length)  ## Will fail if returns True
+    length_check(length, ignore_max_length)
 
     return secrets.token_bytes(length)
 
 
 #### Generate a random text string in hexadecimal (Basicly a wrapper for secrets.token_hex)
 def hex(length: int = 32, ignore_max_length: bool = False):
-
-    #### Check if specified length is allowed
-    length_check(length, ignore_max_length)  ## Will fail if returns True
+    length_check(length, ignore_max_length)
 
     return secrets.token_hex(length)
 
 
 def urlsafe(*args, length: int = 32, ignore_max_length: bool = False):
+    length_check(length, ignore_max_length)
 
-    #### Check if specified length is allowed
-    length_check(length, ignore_max_length)  ## Will fail if returns True
-
-    #### Generate
     generated = secrets.token_urlsafe(length)
 
-    #### Check if 'padding' is defined in 'args'
     if "padding" in args:
         generated += "="  ## Padding is useful when using for encryption
 
-    #### Check if 'encoded' is defined in 'args'
     if "encoded" in args:
         generated = generated.encode()
 
@@ -154,11 +131,8 @@ def urlsafe(*args, length: int = 32, ignore_max_length: bool = False):
 
 #### String from variables
 def var(varstring: str, prefix: Optional[str] = None):
-
-    #### Get today date as most vars use it
     today = datetime.today()
 
-    #### Dictionary with available variables
     vars = {
         "yyyy": today.year,
         "yy": str(today.year)[-2:],
@@ -172,11 +146,9 @@ def var(varstring: str, prefix: Optional[str] = None):
         varstring = varstring[1:]  ## Can't start with %
     generated = ""  ## Empty string to append data to
 
-    #### Loop though the variables
     for var in varstring.split("%"):
         generated += str(vars[var])
 
-    #### Add prefix
     if not prefix == None:
         generated = prefix + generated
 
@@ -204,21 +176,14 @@ def database(cursor, *args, **kwargs):
             "type"
         ]  ## Kwargs needs to be passed to generation functions, type is useless here
 
-    #### * SQL functionality
     if cursor_type == "sql":
-
         id_exists = True
 
-        #### Var method uses a seperate approach
         if method == "var":
-
-            #### Generate the variable ID
             generated_id = var(*args, **kwargs)
             suffix = 1  ## Not ending in 0
 
             while id_exists:
-
-                #### Check if it appears in the database
                 cursor["cursor"].execute(
                     f'SELECT "{ cursor["column"] }" FROM { cursor["table"] } WHERE { cursor["column"] } = "{ generated_id + str(suffix) }"'
                 )
@@ -227,20 +192,16 @@ def database(cursor, *args, **kwargs):
                 else:
                     id_exists = False
 
-            #### Return the generated ID with the suffix
             return generated_id + str(suffix)
 
-        #### If method is not var, generate new until 'id_exists' becomes False
         else:
             while id_exists:
 
-                #### Generate
                 if method == "string":
                     generated_id = string(*args, **kwargs)
                 if method == "integer":
                     generated_id = integer(*args, **kwargs)
 
-                #### Check if it appears in the database
                 cursor["cursor"].execute(
                     f'SELECT "{ cursor["column"] }" FROM { cursor["table"] } WHERE { cursor["column"] } = "{ generated_id }"'
                 )
@@ -250,10 +211,8 @@ def database(cursor, *args, **kwargs):
             return generated_id
 
     elif cursor_type == "mongo":
-
         id_exists = True
 
-        #### Var method uses a seperate approach
         if method == "var":
 
             #### Generate the variable ID
@@ -262,27 +221,20 @@ def database(cursor, *args, **kwargs):
 
             while id_exists:
 
-                #### Check if it appears in the database
                 if cursor["cursor"].find_one({cursor["column"]: generated_id}):
                     suffix += 1  ## If ID already exists add 1 to suffix
                 else:
                     id_exists = False
 
-            #### Return the generated ID with the suffix
             return generated_id + str(suffix)
 
-        #### If method is not var, generate new until 'id_exists' becomes False
         else:
-
             while id_exists:
-
-                #### Generate
                 if method == "string":
                     generated_id = string(*args, **kwargs)
                 if method == "integer":
                     generated_id = integer(*args, **kwargs)
 
-                #### Check if it appears in the database
                 if not cursor["cursor"].find_one({cursor["column"]: generated_id}):
                     id_exists = False
 
