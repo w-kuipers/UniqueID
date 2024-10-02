dc2d45a41fd99796350ec5184af4e0673f867f21
Wibo Kuipers <wibokuip@gmail.com>
2024-10-02 21:26:28 +0200

    Add tests for alpha method

--- src/simpleUID/__init__.py
+++ src/simpleUID/__init__.py
@@ -1,3 +1,4 @@
 from .simpleUID import string, integer, password, database, hex, urlsafe, bytes, var
+from .methods.alpha import alpha
 from .include import length_check
 from .exceptions import length_value_too_long
