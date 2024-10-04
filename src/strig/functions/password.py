ba7fdb9538fa48c6726d4718f287bc2f4d0b35c7
Wibo Kuipers <wibokuip@gmail.com>
2024-10-04 09:47:47 +0200

    Finish password function

--- src/strig/functions/password.py
+++ src/strig/functions/password.py
@@ -1,3 +1,4 @@
+from random import randint, shuffle
 from secrets import choice
 from typing import Optional
 
@@ -26,10 +27,17 @@ def password(
     if symbols:
         charset += SYMBOLS + SYMBOLS
 
-    generated = "".join(choice(charset) for _ in range(length))
+    generated_chars = [choice(charset) for _ in range(length)]
 
     ## Ensure there is always a character present from
     ## every enabled characterset
+    if digits:
+        generated_chars[randint(0, len(generated_chars) - 1)] = choice(NUMERIC)
+    if symbols:
+        generated_chars[randint(0, len(generated_chars) - 1)] = choice(SYMBOLS)
+
+    shuffle(generated_chars)
+    generated = "".join(generated_chars)
 
     if not prefix == None:
         generated = prefix + generated
