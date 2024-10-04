4d284c79ec06e44df47a10c9977fa45be5c0d314
Wibo Kuipers <wibokuip@gmail.com>
2024-10-04 08:42:33 +0200

    Add password function

---
+++ src/strig/functions/password.py
@@ -0,0 +1,37 @@
+from secrets import choice
+from typing import Optional
+
+from ..constants import ALPHA, ALPHA_LOWER, ALPHA_UPPER, NUMERIC, SYMBOLS
+from ..types import Case
+
+
+def password(
+    length: int = 20,
+    prefix: Optional[str] = None,
+    case: Case = None,
+    digits=True,
+    symbols=True,
+):
+    """Generate a cryptographically secure random password"""
+
+    charset = ALPHA
+    if case == "upper":
+        charset = ALPHA_UPPER
+    if case == "lower":
+        charset = ALPHA_LOWER
+
+    if digits:
+        charset += NUMERIC
+
+    if symbols:
+        charset += SYMBOLS + SYMBOLS
+
+    generated = "".join(choice(charset) for _ in range(length))
+
+    ## Ensure there is always a character present from
+    ## every enabled characterset
+
+    if not prefix == None:
+        generated = prefix + generated
+
+    return generated
