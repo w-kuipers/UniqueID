b27f8e772073fb817c936baa806813965806ee96
Wibo Kuipers <wibokuip@gmail.com>
2024-10-05 17:49:42 +0200

    Add some variables

--- src/strig/functions/var_methods/os.py
+++ src/strig/functions/var_methods/os.py
@@ -3,4 +3,9 @@ import platform
 class OsMethods:
     def system(self): 
         return platform.system()
-        
+
+    def release(self): 
+        return platform.release()        
+
+    def version(self): 
+        return platform.version()        
